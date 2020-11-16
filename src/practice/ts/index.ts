6bfe42d945987b3e252ed954a157d351d746d489
fed-gren <deveb22@gmail.com>
2020-11-17 00:47:23 +0900

    chore: tab 관련 스켈레톤 코드 작성
    - 연습 기능 확인을 위한 tab 기능 관련 스켈레톤 코드(주석?) 작성

--- src/practice/ts/index.ts
+++ src/practice/ts/index.ts
@@ -1,3 +1,19 @@
-const add = (number1: number, number2: number) => number1 + number2;
+/**
+ * tab 기능 구현
+ *
+ * 각 tab 버튼을 클릭하면 해당 tab을 표시해준다.
+ * 각 tab 버튼을 클릭하면 각 관련된 컨텐츠를 표시한다. (index로 구분)
+ */
 
-console.log(add(1, 2));
+const classNames = {
+  TAB: "tab",
+  CONTENT: "content"
+};
+
+const createTab = (el: HTMLElement) => {
+  // element 캐싱 (tab, content)
+
+  // tab 버튼 클릭 이벤트
+
+  // content 노출 핸들러
+}
