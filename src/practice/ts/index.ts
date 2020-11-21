f20c86a2d59385c74db0f5c6a55786b4c5f8c43f
fed-gren <deveb22@gmail.com>
2020-11-21 23:51:54 +0900

    feat: tab 기능 구현
    - 클릭한 tab index에 맞는 content 노출
    - content 노출 시 class hide로 제어

--- src/practice/ts/index.ts
+++ src/practice/ts/index.ts
@@ -6,14 +6,41 @@
  */
 
 const classNames = {
+  TAB_ROOT: "tab-root",
   TAB: "tab",
   CONTENT: "content"
 };
 
-const createTab = (el: HTMLElement) => {
-  // element 캐싱 (tab, content)
+const getSelector = (className: string) => `.${className}`;
 
-  // tab 버튼 클릭 이벤트
+const createTab = (tabRootElement: HTMLElement) => {
+  // element 캐싱 (tab, content)
+  const { TAB, CONTENT } = classNames;
+  const tabElements = Array.from(tabRootElement.querySelectorAll(getSelector(TAB)));
+  const contentElements = Array.from(tabRootElement.querySelectorAll(getSelector(CONTENT)));
+
+  const checkTargetIsTabElement = (target: HTMLElement, index: number) => target !== null && index >= 0;
+  const hideAllContents = () => {
+    contentElements.forEach(contentElement => contentElement.classList.add("hide"));
+  }
+  const showContent = (contentElement: Element) => contentElement.classList.remove("hide");
 
   // content 노출 핸들러
+  const showMatchedContent = (evt: Event) => {
+    const target = evt.target as HTMLElement;
+    const targetIndex = tabElements.indexOf(target);
+
+    if (!checkTargetIsTabElement(target, targetIndex)) return;
+    evt.preventDefault();
+
+    hideAllContents();
+    showContent(contentElements[targetIndex]);
+  }
+
+  // tab 버튼 클릭 이벤트
+  tabRootElement.addEventListener("click", showMatchedContent);
 }
+
+const rootElement = document.querySelector(getSelector(classNames.TAB_ROOT)) as HTMLElement;
+
+createTab(rootElement);
