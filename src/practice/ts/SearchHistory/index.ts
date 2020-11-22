eec4577946c323d7935e0c058c882dff966b2194
fed-gren <deveb22@gmail.com>
2020-11-22 23:07:30 +0900

    feat: setList 기능 구현
    - hisoty 데이터를 활용하여 item list 노출

--- src/practice/ts/SearchHistory/index.ts
+++ src/practice/ts/SearchHistory/index.ts
@@ -1,20 +1,51 @@
 import { classNames } from "./constants";
 
+
+
+interface history {
+  url: string;
+  timestamp: string;
+  title: string;
+}
+
+interface histories {
+  [key: string]: history;
+}
+
+
 export default class SearchHistory {
   private baseElement: HTMLElement;
   private inputElement: HTMLInputElement;
   private listElement: HTMLUListElement;
   private listItemElement: HTMLLIElement;
-  private historyJson: JSON | null;
+  private historyJson: histories;
 
   constructor(baseElement: HTMLElement) {
     this.baseElement = baseElement;
     this.listElement = this.baseElement.querySelector(`.${classNames.HISTORY_LIST}`) as HTMLUListElement;
     this.inputElement = this.baseElement.querySelector(`.${classNames.SEARCH_INPUT}`) as HTMLInputElement;
     this.listItemElement = this.baseElement.querySelector(`.${classNames.HISTORY_ITEM}`) as HTMLLIElement;
-    this.historyJson = null;
+    this.historyJson = {};
 
-    this.fetchHistoryItems();
+    this.init();
+  }
+
+  async init() {
+    await this.fetchHistoryItems();
+    this.setList();
+  }
+
+  setList() {
+    /**
+     * history list 밑에 li 템플릿으로 추가
+     */
+    [...Object.values(this.historyJson)].forEach(history => {
+      const itemElement = document.createElement("li");
+
+      itemElement.classList.add(classNames.HISTORY_ITEM);
+      itemElement.textContent = history.title;
+      this.listElement.appendChild(itemElement);
+    });
   }
 
   async fetchHistoryItems() {
@@ -22,9 +53,8 @@ export default class SearchHistory {
      * TODO: 지금은 mock api에서 가져오지만 추후엔 local storage에서 가져와야 함
      */
     await fetch("http://localhost:8080/history")
-      .then(response => {
-        return response.json();
-      }).then(historyJson => {
+      .then(response => response.json())
+      .then(historyJson => {
         this.historyJson = historyJson;
       });
   }
