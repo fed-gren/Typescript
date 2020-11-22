1695a488ce2d2273dc65732857b3c73267f6864b
fed-gren <deveb22@gmail.com>
2020-11-22 22:36:39 +0900

    feat: fetch history

--- src/practice/ts/SearchHistory/index.ts
+++ src/practice/ts/SearchHistory/index.ts
@@ -5,12 +5,27 @@ export default class SearchHistory {
   private inputElement: HTMLInputElement;
   private listElement: HTMLUListElement;
   private listItemElement: HTMLLIElement;
+  private historyJson: JSON | null;
 
   constructor(baseElement: HTMLElement) {
     this.baseElement = baseElement;
     this.listElement = this.baseElement.querySelector(`.${classNames.HISTORY_LIST}`) as HTMLUListElement;
     this.inputElement = this.baseElement.querySelector(`.${classNames.SEARCH_INPUT}`) as HTMLInputElement;
     this.listItemElement = this.baseElement.querySelector(`.${classNames.HISTORY_ITEM}`) as HTMLLIElement;
+    this.historyJson = null;
+
+    this.fetchHistoryItems();
   }
 
+  async fetchHistoryItems() {
+    /**
+     * TODO: 지금은 mock api에서 가져오지만 추후엔 local storage에서 가져와야 함
+     */
+    await fetch("http://localhost:8080/history")
+      .then(response => {
+        return response.json();
+      }).then(historyJson => {
+        this.historyJson = historyJson;
+      });
+  }
 }
