06f9af847aeee1c3bdf276b40ea4febc36ec9fb0
박상규 <park78951@gmail.com>
2020-11-30 23:45:33 +0900

    style: 주석 제거

--- src/practice/ts/components/StoreHistory/index.ts
+++ src/practice/ts/components/StoreHistory/index.ts
@@ -1,16 +1,3 @@
-/**
- * 필요 기능
- * 1. url을 입력 가능한 input 추가
- * 2. 추가된 url이 시간순서대로 위에서 아래로 쌓임
- */
-
-/**
- * 구조
- * 1. 전체적으로 관리하는 root class (기능 구현 포함)
- * 2. history url들을 관리하는 class
- * 3. ui 담당하는 class
- */
-
 import HistoryManager from "./HistoryManager";
 import HistoryStore from "./HistoryStore";
 
